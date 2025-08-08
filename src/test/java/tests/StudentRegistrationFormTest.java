47a518bedcd8461a22a3b2f88a5e1f30bca2aaea
sergejgluhov <arane1223@gmail.com>
2025-08-08 12:04:13 +0500

    added build.gradle, StudentRegistrationFormTest

---
+++ src/test/java/tests/StudentRegistrationFormTest.java
@@ -0,0 +1,46 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class StudentRegistrationFormTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+     void fillFormTest() {
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        $("#firstName").setValue("Alex");
+        $("#lastName").setValue("Terrible");
+        $("#userEmail").setValue("test@test.com");
+        $("#genterWrapper").$(byText("Other")).click();
+        $("#userNumber").setValue("0123456789");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").$(byText("May")).click();
+        $(".react-datepicker__year-select").$(byText("1995")).click();
+        $(".react-datepicker__month").$(byText("15")).click();
+        $("#subjectsInput").setValue("Math").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#hobbiesWrapper").$(byText("Music")).click();
+        $("#uploadPicture").uploadFromClasspath("Sega.jpg");
+        $("#currentAddress").setValue("Ekaterinberg");
+        $("#stateCity-wrapper").scrollTo();
+        $("#react-select-3-input").setValue("NCR").pressEnter();
+        $("#react-select-4-input").setValue("Delhi").pressEnter();
+        $("#submit").click();
+        $(".modal-content").shouldHave(text("Thanks for submitting the form"));
+    }
+}
